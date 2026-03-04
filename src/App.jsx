c94696b9e0345a72fbad173b0ea01d47edb358fc
manoj <manoj.l2k01@gmail.com>
2026-03-04 21:44:51 +0530

    implemented contact section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Cocktails from './components/Cocktails.jsx'
 import About from './components/About.jsx'
 import Art from './components/Art.jsx'
 import Menu from './components/Menu.jsx'
-// import Contact from './components/Contact.jsx'
+import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -20,7 +20,7 @@ const App = () => {
 	 <About />
 	 <Art />
 	 <Menu />
-	 {/* <Contact /> */}
+	 <Contact />
 	</main>
  )
 }
