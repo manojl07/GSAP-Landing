b9b643c4a88412661e2fe9dd41e323decddb4789
manoj <manoj.l2k01@gmail.com>
2026-03-04 21:43:19 +0530

    implemented menu section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from './components/Hero.jsx'
 import Cocktails from './components/Cocktails.jsx'
 import About from './components/About.jsx'
 import Art from './components/Art.jsx'
-// import Menu from './components/Menu.jsx'
+import Menu from './components/Menu.jsx'
 // import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -19,8 +19,8 @@ const App = () => {
 	 <Cocktails />
 	 <About />
 	 <Art />
-	 {/* <Menu />
-	 <Contact /> */}
+	 <Menu />
+	 {/* <Contact /> */}
 	</main>
  )
 }
